99459cd1866bda44df72c4a1fcbb6b25dd2c0155
liuguili <gongzili456@gmail.com>
2017-12-13 11:13:11 +0800

    add socket.io support

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import boom from 'boom'
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
 import Debug from 'debug'
+import http from 'http'
+import Socket from 'socket.io'
 import router from './routes'
 // import filters from './filters'
 const debug = new Debug('app:index:')
@@ -11,19 +13,19 @@ const APP = new Koa()
 // Error Handler
 APP.use((ctx, next) =>
   next()
-    .then(() => {
-      if (ctx.status >= 400) {
-        debug('ctx.status: ', ctx.status)
-        return ctx.body = boom.create(ctx.status).output.payload
-      }
-      return false
-    })
-    .catch(e => {
-      debug('app error: ', e.status, ',', e.message, ',', e.stack)
-      const status = e.status || 500
-      ctx.status = status
-      ctx.body = boom.create(status, e.message).output.payload
-    })
+  .then(() => {
+    if (ctx.status >= 400) {
+      debug('ctx.status: ', ctx.status)
+      return ctx.body = boom.create(ctx.status).output.payload
+    }
+    return false
+  })
+  .catch(e => {
+    debug('app error: ', e.status, ',', e.message, ',', e.stack)
+    const status = e.status || 500
+    ctx.status = status
+    ctx.body = boom.create(status, e.message).output.payload
+  })
 )
 
 // User log
@@ -40,4 +42,13 @@ APP.use(bodyParser())
 APP.use(router().routes())
 APP.use(router().allowedMethods())
 
-export default APP
+const server = http.createServer(APP.callback())
+const io = new Socket()
+
+io.on('connection', socket => {
+  debug('A socket client connected.')
+})
+
+APP.context.io = io
+
+export default server
